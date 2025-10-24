e068048ee9f5b38cd5237796421d3d6decd93536
Edgar <edr9719@outlook.com>
2025-10-23 21:48:13 -0600

    Added About page and coin-details

---
+++ src/pages/coin-details.jsx
@@ -0,0 +1,32 @@
+import { useParams } from 'react-router';
+import { useState, useEffect } from 'react';
+const API_URL = import.meta.env.VITE_COIN_API_URL;
+
+const CoinDetailsPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const res = await fetch(`${API_URL}/${id}`);
+        if (!res.ok) throw new Error('Failed to fetch data');
+        const data = res.json();
+        console.log(data);
+        setCoin(data);
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoin();
+  }, [id]);
+
+  return <div>Coin Details {id}</div>;
+};
+
+export default CoinDetailsPage;
